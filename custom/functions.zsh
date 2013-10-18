4089da8745edf042e645e1a34a79405743a0e063
farthen <monkey@farthen.de>
2013-10-19 00:06:28 +0200

    Revert last commit. I fail. This was a stupid idea

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -1,5 +1,5 @@
 function finderpath() {
-    echo \"$(/usr/bin/osascript << EOF
+    echo $(/usr/bin/osascript << EOF
     tell application "Finder"
         if current view of window 1 is in {list view, flow view} and selection is not {} then
             set i to item 1 of (get selection)
@@ -13,9 +13,9 @@ function finderpath() {
         end if
         return POSIX path of (p as alias)
     end tell
-    EOF)\"
+    EOF)
 }
 
 function cdf() {
-    cd $(finderpath)
+    cd "$(finderpath)"
 }
